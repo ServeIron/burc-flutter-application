686422018ba3f1b50031570276d0226aed536f8b
ServeIron <servetgkdmr@gmail.com>
2023-09-14 16:42:56 +0300

    dosyalar eklendi

---
+++ lib/burc_listesi.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class BurcListesi extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Burçlar Listesi")),
+      body: Center(child: Text("Burç listesi buraya gelecek")),
+    );
+  }
+}
